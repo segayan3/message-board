a38766b466367303ffcd8677265ff857adf49ed9
NaohiroSegawa <segayan3@gmail.com>
2017-01-13 12:27:14 +0000

    create message model

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,20 @@
 class MessagesController < ApplicationController
   def index
+    # モデルインスタンスを初期化
+    @message = Message.new
+    
+    # モデルから全データを取得
+    @messages = Message.all
+  end
+  
+  def create
+    @message = Message.new(message_params)
+    @message.save
+    redirect_to root_path, notice: 'メッセージを保存しました'
+  end
+  
+  private
+  def message_params
+    params.require(:message).permit(:name, :body)
   end
 end
