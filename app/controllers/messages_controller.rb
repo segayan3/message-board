78eab44d969f1a313df3f5d39df4b030972cb239
NaohiroSegawa <segayan3@gmail.com>
2017-01-23 13:50:26 +0000

    Lesson6-9

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -31,7 +31,6 @@ class MessagesController < ApplicationController
   
   def create
     @message = Message.new(message_params)
-    binding.pry
     if @message.save
       redirect_to root_path, notice: 'メッセージを保存しました'
     else
